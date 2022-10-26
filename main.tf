393131cbb7f02fe29c4b7c962d1640e987d409e1
root <root@ip-172-31-0-6.ap-south-1.compute.internal>
2022-10-26 06:50:35 +0000

    modified

---
+++ main.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+    access_key = "AKIA5FIOCDDBSVPXRMXJ"
+    secret_key = "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"
+    region = "ap-south-1"
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = " AKIA5FIOCDDBSVPXRMXJ "
+}
+variable "aws_secret_key" {
+default = " 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 "
+ }
+variable "region" {
+    default = "ap-south-1"
+}
